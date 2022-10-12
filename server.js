fcc3c5ab633ecb917429d616251fabd42070182d
Patrick M <patrickmacdonald07@gmail.com>
2022-10-12 17:33:19 -0400

    connected to mysql database

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+const mysql = require("mysql2");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "ArgenTina13#!",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
 app.use((req, res) => {
   res.status(404).end();
